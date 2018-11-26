2c6110ee1c77258694b20892a89db37f5b4ade19
Pushkar <pushkar2gr8@gmail.com>
2018-11-26 23:08:08 +0530

    deployment updates

---
+++ build/precache-manifest.9ef56398a5792335ffdb64b22887a299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ad0bf4bf2f4a88582d7",
+    "url": "/pushkar2gr8/aiesec_challenge/tree/gh-pages/index.html/static/js/runtime~main.6ad0bf4b.js"
+  },
+  {
+    "revision": "f4feb128bf8a9d26270f",
+    "url": "/pushkar2gr8/aiesec_challenge/tree/gh-pages/index.html/static/js/main.f4feb128.chunk.js"
+  },
+  {
+    "revision": "697ed8e1b2083aea2b5d",
+    "url": "/pushkar2gr8/aiesec_challenge/tree/gh-pages/index.html/static/js/1.697ed8e1.chunk.js"
+  },
+  {
+    "revision": "f4feb128bf8a9d26270f",
+    "url": "/pushkar2gr8/aiesec_challenge/tree/gh-pages/index.html/static/css/main.9e02ac53.chunk.css"
+  },
+  {
+    "revision": "697ed8e1b2083aea2b5d",
+    "url": "/pushkar2gr8/aiesec_challenge/tree/gh-pages/index.html/static/css/1.468c7371.chunk.css"
+  },
+  {
+    "revision": "8a13dbcaf1ee0d8c3fc05b663b77a759",
+    "url": "/pushkar2gr8/aiesec_challenge/tree/gh-pages/index.html/index.html"
+  }
+];
